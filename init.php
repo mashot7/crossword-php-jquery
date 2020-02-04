24f08028477cb7631efb0aeafeae8a739f92bf74
mashot7 <mashot2@gmail.com>
2020-02-04 18:04:01 +0400

    Fix code

--- init.php
+++ init.php
@@ -3,10 +3,10 @@
 require 'lib/Crossword.php';
 require 'lib/ArrayFilter.php';
 
-$cols = isset($_REQUEST['cols']) ? (int) $_REQUEST['cols'] : 20;
-$rows = isset($_REQUEST['rows']) ? (int) $_REQUEST['rows'] : 20;
-$max_words = isset($_REQUEST['max_words']) ? (int) $_REQUEST['max_words'] : 20;
-$max_tries = isset($_REQUEST['max_tries']) ? (int) $_REQUEST['max_tries'] : 15;
+$cols = 20;
+$rows = 20;
+$max_words = 20;
+$max_tries = 15;
 
 $pc = new Crossword($rows, $cols, $words);
 
